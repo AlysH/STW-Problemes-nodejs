d16d2c0b71fe5ee3e3bdc2d1fa70978cde1c01ec
Paula Sarqui <pfsarqui@gmail.com>
2021-02-23 10:11:49 +0100

    Implements exercise 11
    I have yet to do exercise 10

--- Exercici_11.js
+++ Exercici_11.js
@@ -1,15 +1,26 @@
+const fs = require('fs');
+
 function asyncMap(list, f, callback2) {
+    let resultList = [];
 
+    let myPromise = new Promise((resolve, reject) => {
+        list.map((file, index, array) => f('./' + file, 'utf8', (err, data) => {
+            if (err) {
+                callback2(err);
+                throw err;
+            }
+
+            resultList.push(data);
+
+            if (index === (array.length - 1)) {
+                resolve();
+            }
+        }));
+    });
+
+    myPromise.then(() => {
+        callback2(resultList);
+    });
 }
 
-function callback2(err, resultList) {
-
-}
-
-function f(a, callback1) {
-
-}
-
-function callback1(err, result) {
-
-}
+asyncMap(['a1.txt', 'a2.txt'], fs.readFile, function (a) { console.log(a) });
