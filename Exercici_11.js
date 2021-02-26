b2e186819a677dfe52c4685bc68f885f0c679b0e
Paula Sarqui <pfsarqui@gmail.com>
2021-02-26 14:05:41 +0100

    Eliminates promise from exercise 11
    I have yet to do exercise 10 and 14

--- Exercici_11.js
+++ Exercici_11.js
@@ -3,24 +3,20 @@ const fs = require('fs');
 function asyncMap(list, f, callback2) {
     let resultList = [];
 
-    let myPromise = new Promise((resolve, reject) => {
-        list.map((file, index, array) => f('./' + file, 'utf8', (err, data) => {
-            if (err) {
-                callback2(err);
-                throw err;
-            }
+    let callback1 = function (err, data) {
+        if (err) {
+            callback2(err);
+            throw err;
+        }
 
-            resultList.push(data);
+        resultList.push(data);
 
-            if (index === (array.length - 1)) {
-                resolve();
-            }
-        }));
-    });
+        if (resultList.length === list.length) {
+            callback2(resultList);
+        }
+    };
 
-    myPromise.then(() => {
-        callback2(resultList);
-    });
+    list.map((file, index, array) => f('./' + file, 'utf8', (err, data) => callback1(err, data)));
 }
 
-asyncMap(['a1.txt', 'a2.txt'], fs.readFile, function (a) { console.log(a) });
+asyncMap(['a1.txt'], fs.readFile, function (a) { console.log(a) });
