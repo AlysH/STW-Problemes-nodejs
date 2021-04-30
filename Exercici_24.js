1e531b92596f1e2cab26afa4ec3823ab93865525
Paula Sarqui <pfsarqui@gmail.com>
2021-04-30 14:00:21 +0200

    Explains 'Exercici_24'.
    TODO:
    - Exercise 10.

---
+++ Exercici_24.js
@@ -0,0 +1,12 @@
+let antipromise = function (promise) {
+    return new Promise((resolve, reject) => {
+        promise.then((x) => {
+            reject(x);
+        }).catch((x) => {
+            resolve(x);
+        });
+    });
+};
+
+antipromise(Promise.reject(0)).then((x) => { console.log("Antipromise resolved, x = " + x) });
+antipromise(Promise.resolve(1)).catch((x) => { console.log("Antipromise rejected, x = " + x) });
