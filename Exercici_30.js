eea53e8391d69b4ba65ea550bade1a30ca9669c7
Paula Sarqui <pfsarqui@gmail.com>
2021-04-30 17:59:11 +0200

    Changer minor details in format.
    TODO:
    - Exercise 10.

--- Exercici_30.js
+++ Exercici_30.js
@@ -8,9 +8,12 @@ let functionPromiseComposer = function (f1, f2) {
     };
 };
 
-let f1 = x => new Promise((resolve, reject) => resolve(x + 1));
-let f2 = x => new Promise((resolve, reject) => reject('always fails'));
-let f3 = x => new Promise((resolve, reject) => setTimeout(() => resolve(x * 2), 500));
+let f1 = x => new Promise((resolve, reject) =>
+    resolve(x + 1));
+let f2 = x => new Promise((resolve, reject) =>
+    reject('always fails'));
+let f3 = x => new Promise((resolve, reject) =>
+    setTimeout(() => resolve(x * 2), 500));
 
 functionPromiseComposer(f1, f1)(3).then(console.log);
 functionPromiseComposer(f1, f2)(3).catch(console.log);
