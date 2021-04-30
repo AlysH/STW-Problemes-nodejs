3a2e2a6f8848bccae9beccbf56110b12784344f6
Paula Sarqui <pfsarqui@gmail.com>
2021-04-30 14:33:58 +0200

    Explains 'Exercici_27'.
    TODO:
    - Exercise 10.

---
+++ Exercici_27.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+
+let callbackToPromise = function (f) {
+    return (function (file) {
+        return new Promise((resolve, reject) => {
+            f(file, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    });
+};
+
+let readToPromise2 = callbackToPromise(fs.readFile);
+
+readToPromise2("a1.txt").then(x => console.log("Contents: ", x)).catch(x => console.log("Error: ", x))
