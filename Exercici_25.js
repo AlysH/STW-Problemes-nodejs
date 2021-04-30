7113e9438fa02f9c035cd015b56d1279dbca0265
Paula Sarqui <pfsarqui@gmail.com>
2021-04-30 14:16:03 +0200

    Explains 'Exercici_25'.
    TODO:
    - Exercise 10.

---
+++ Exercici_25.js
@@ -0,0 +1,16 @@
+let promiseToCallback = function (f) {
+    return (function (x, callback) {
+        f(x).then((res) => callback(null, res), (res) => callback(res, null));
+    });
+};
+
+let isEven = x => new Promise((resolve, reject) => {
+    x % 2 ? reject(x) : resolve(x);
+});
+
+let isEvenCallback = promiseToCallback(isEven);
+
+isEven(2).then(() => console.log("OK"), () => console.log("KO"));
+isEvenCallback(2, (err, res) => console.log(err, res));
+isEven(3).then(() => console.log("OK"), () => console.log("KO"));
+isEvenCallback(3, (err, res) => console.log(err, res));
